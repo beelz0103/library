1a3dc60074dd2a276d382aa86ef1fa0324f7f64e
praveen <anabathula.praveen103@gmail.com>
2023-01-20 14:18:33 +0530

    Update readme

--- README.md
+++ README.md
@@ -1,30 +1,5 @@
-# The Odin Project Library Project
-## Project: Library
-  A small Library app, created with **HTML**, **CSS** and **JavaScript**. The data is stored in **localStorage**. The project is from The Odin Project [curriculum](https://www.theodinproject.com/courses/javascript/lessons/library).
+# Library
 
-🔗 **Live preview** of the project is [here](https://beelz0103.github.io/library/).
+A simple Library app, created with **HTML**, **CSS** and **JavaScript**. Implemented object-oriented programming principles and used local storage to store the data.
 
-### Overview
-#### **Features:**
-* Data is **saved** in `localStorage`
-* You can **add a book**
-* Before adding a book, you can **mark if it's already read or not**
-* You can **change book status** after addition
-* You can **delete a particular book**
-
-#### **Tools:**
-* Visual Studio Code
-* Linux terminal
-* Git and GitHub
-
-#### **Third party code:**
-* [Font Awesome Icons](https://fontawesome.com/)
-
-#### **Outcome:**
-* Used **HTML5 semantic elements** for better structure and readability
-* Used **CSS Grid** module to create layout
-* Learned how to **create a custom checkbox**
-* Learned how to **create a modal**
-* Learned how to **save data to** `localStorage`
-* Deepened knowledge of **DOM manipulation**, **HTML structure**, **CSS styling subtleties** and **JS functionality** in general
-* Had lots of fun with styling! 🎨
+🔗 Preview it live [here](https://beelz0103.github.io/library/).
--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 let myLibrary = [];
-let book_id = 0
+let book_id = 0;
 
 class Book {
   constructor(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-    this.identifier = null
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.identifier = null;
   }
 }
 
-document.querySelector("#addbook").addEventListener("click", showForm)
+document.querySelector("#addbook").addEventListener("click", showForm);
 document.querySelector("#book_closebutton").addEventListener("click", hideForm);
-document.querySelector("#book_createbook").addEventListener("click", createBook)
+document
+  .querySelector("#book_createbook")
+  .addEventListener("click", createBook);
 
 function showForm() {
   document.querySelector(".modal-form").classList.add("show-modal");
@@ -24,116 +26,125 @@ function hideForm() {
 }
 
 function currentBook() {
-  return myLibrary.at(-1)
-} 
+  return myLibrary.at(-1);
+}
 
-function createBook() {  
-  const booktitle = document.querySelector("#book_title")
-  const bookauthor = document.querySelector("#book_author")
-  const bookpages = document.querySelector("#book_pages")
-  const bookread = document.querySelector("#book_read")
-  if (checkvalidity(booktitle, bookauthor, bookpages)) { 
-    myLibrary.push(new Book(booktitle.value, bookauthor.value, bookpages.value, bookread.checked))       
-    currentBook().identifier = book_id.toString()
-    book_id += 1  
-    buildCard(currentBook())
-    hideForm() 
-    document.querySelector("form").reset()  
+function createBook() {
+  const booktitle = document.querySelector("#book_title");
+  const bookauthor = document.querySelector("#book_author");
+  const bookpages = document.querySelector("#book_pages");
+  const bookread = document.querySelector("#book_read");
+  if (checkvalidity(booktitle, bookauthor, bookpages)) {
+    myLibrary.push(
+      new Book(
+        booktitle.value,
+        bookauthor.value,
+        bookpages.value,
+        bookread.checked
+      )
+    );
+    currentBook().identifier = book_id.toString();
+    book_id += 1;
+    buildCard(currentBook());
+    hideForm();
+    document.querySelector("form").reset();
   }
 }
 
-function checkvalidity (title, author, read) {
-  if (title.value == "") title.reportValidity()
-  else if (author.value == "") author.reportValidity()
-  else if (read.value == "") read.reportValidity()
-  else return true
+function checkvalidity(title, author, read) {
+  if (title.value == "") title.reportValidity();
+  else if (author.value == "") author.reportValidity();
+  else if (read.value == "") read.reportValidity();
+  else return true;
 }
 
 function buildCard(value) {
-  const card_container = document.querySelector("div#cardcontainer")
-  const card_div = document.createElement("div")
-  card_div.id = value.identifier
+  const card_container = document.querySelector("div#cardcontainer");
+  const card_div = document.createElement("div");
+  card_div.id = value.identifier;
 
-  const title_div = document.createElement('div')
-  title_div.textContent = `Title: ${value.title}`
-  card_div.appendChild(title_div)
+  const title_div = document.createElement("div");
+  title_div.textContent = `Title: ${value.title}`;
+  card_div.appendChild(title_div);
 
-  const auth_div = document.createElement('div')
-  auth_div.textContent = `Author: ${value.author}`
-  card_div.appendChild(auth_div)
+  const auth_div = document.createElement("div");
+  auth_div.textContent = `Author: ${value.author}`;
+  card_div.appendChild(auth_div);
 
-  const page_div = document.createElement('div')
-  page_div.textContent = `Pages: ${value.pages}`
-  card_div.appendChild(page_div)
+  const page_div = document.createElement("div");
+  page_div.textContent = `Pages: ${value.pages}`;
+  card_div.appendChild(page_div);
 
-  const read_btn = document.createElement("button")
-  
-  if (value.read ) {
-    read_btn.textContent = "Read"
-    read_btn.style.backgroundColor = "green"
+  const read_btn = document.createElement("button");
+
+  if (value.read) {
+    read_btn.textContent = "Read";
+    read_btn.style.backgroundColor = "green";
+  } else {
+    read_btn.textContent = "Not Read";
+    read_btn.style.backgroundColor = "red";
   }
-  else {
-    read_btn.textContent = "Not Read"
-    read_btn.style.backgroundColor = "red"
-  }
 
-  read_btn.addEventListener("click", changeReadStatus)
-  card_div.appendChild(read_btn)  
+  read_btn.addEventListener("click", changeReadStatus);
+  card_div.appendChild(read_btn);
 
-  const delete_btn = document.createElement("button")  
-  delete_btn.textContent = "Delete"
-  delete_btn.addEventListener("click", deleteBook)
-  card_div.appendChild(delete_btn)  
+  const delete_btn = document.createElement("button");
+  delete_btn.textContent = "Delete";
+  delete_btn.addEventListener("click", deleteBook);
+  card_div.appendChild(delete_btn);
 
-  card_container.appendChild(card_div)
+  card_container.appendChild(card_div);
 }
 
-
-function deleteBook(e) {  
-  myLibrary = myLibrary.filter(book => book.identifier != e.target.parentNode.id)
-  e.target.parentNode.remove();    
+function deleteBook(e) {
+  myLibrary = myLibrary.filter(
+    (book) => book.identifier != e.target.parentNode.id
+  );
+  e.target.parentNode.remove();
 }
 
-function changeReadStatus(e) { 
-  let book = myLibrary.find(element => element.identifier === e.target.parentNode.id);  
+function changeReadStatus(e) {
+  let book = myLibrary.find(
+    (element) => element.identifier === e.target.parentNode.id
+  );
   if (book.read) {
-    book.read = false
-    e.target.textContent = "Not Read"
-    e.target.style.backgroundColor = "red"
+    book.read = false;
+    e.target.textContent = "Not Read";
+    e.target.style.backgroundColor = "red";
+  } else {
+    book.read = true;
+    e.target.textContent = "Read";
+    e.target.style.backgroundColor = "green";
   }
-  else {
-    book.read = true
-    e.target.textContent = "Read"
-    e.target.style.backgroundColor = "green"
-  }   
 }
 
-function saveLibrary(){
+function saveLibrary() {
   localStorage.clear();
   localStorage.setItem(`libr`, JSON.stringify(myLibrary));
 }
 
-function loadLibrary(){
+function loadLibrary() {
   newLib = JSON.parse(localStorage.getItem(`libr`));
   if (newLib == null) {
-    return
-  }
-  else {
+    return;
+  } else {
     if (newLib.length != 0) {
-      book_id = parseInt(newLib.at(-1).identifier) + 1
+      book_id = parseInt(newLib.at(-1).identifier) + 1;
     }
 
     newLib.forEach((bookObj) => {
-      myLibrary.push(new Book(bookObj.title, bookObj.author, bookObj.pages, bookObj.read));
-      currentBook().identifier = bookObj.identifier
-      buildCard(currentBook())
-    })
+      myLibrary.push(
+        new Book(bookObj.title, bookObj.author, bookObj.pages, bookObj.read)
+      );
+      currentBook().identifier = bookObj.identifier;
+      buildCard(currentBook());
+    });
   }
 }
 
 window.addEventListener("beforeunload", () => {
-  saveLibrary()
+  saveLibrary();
   return null;
-})
+});
 
 loadLibrary();
